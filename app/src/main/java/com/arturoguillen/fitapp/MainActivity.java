c9705b99f4963d957019ec6bdb0488f98864bd40
Arturo Guillen <sammyjankis87@gmail.com>
2017-06-12 19:38:31 +0200

    Added basic MainActivity

---
+++ app/src/main/java/com/arturoguillen/fitapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.arturoguillen.fitapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by agl on 11/06/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
