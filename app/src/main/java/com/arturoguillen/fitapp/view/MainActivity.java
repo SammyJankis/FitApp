8ec2d47753910d38e9a84cb864fb95dfe08def3b
Arturo Guillen <sammyjankis87@gmail.com>
2017-06-12 23:40:21 +0200

    Added permissions error flow

--- app/src/main/java/com/arturoguillen/fitapp/view/MainActivity.java
+++ app/src/main/java/com/arturoguillen/fitapp/view/MainActivity.java
@@ -1,13 +1,18 @@
 package com.arturoguillen.fitapp.view;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 
 import com.arturoguillen.fitapp.R;
 import com.arturoguillen.fitapp.di.FitComponent;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
@@ -24,7 +29,7 @@ import javax.inject.Inject;
  * Created by agl on 11/06/2017.
  */
 
-public class MainActivity extends InjectedActivity implements GoogleApiClient.ConnectionCallbacks {
+public class MainActivity extends InjectedActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     public static final String TAG = "MainActivity";
     private static int REQUEST_CODE_RESOLVE_ERR = 1000;
@@ -67,16 +72,32 @@ public class MainActivity extends InjectedActivity implements GoogleApiClient.Co
     }
 
     private void buildFitnessClient() {
-        if (googleApiClient != null && !googleApiClient.isConnectionCallbacksRegistered(this)) {
+        if (googleApiClient != null &&
+                !googleApiClient.isConnectionCallbacksRegistered(this) &&
+                !googleApiClient.isConnectionFailedListenerRegistered(this)) {
             googleApiClient.registerConnectionCallbacks(this);
+            googleApiClient.registerConnectionFailedListener(this);
             googleApiClient.connect(GoogleApiClient.SIGN_IN_MODE_OPTIONAL);
         }
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        buildFitnessClient();
+    }
+
+   @Override
     protected void onPause() {
         super.onPause();
-        googleApiClient.unregisterConnectionCallbacks(this);
+       if (googleApiClient.isConnected())
+           googleApiClient.disconnect();
+
+       if (googleApiClient.isConnectionCallbacksRegistered(this))
+           googleApiClient.unregisterConnectionCallbacks(this);
+
+       if (googleApiClient.isConnectionFailedListenerRegistered(this))
+           googleApiClient.unregisterConnectionFailedListener(this);
     }
 
     @Override
@@ -92,12 +113,6 @@ public class MainActivity extends InjectedActivity implements GoogleApiClient.Co
         } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
             Log.i(TAG, "Connection lost.  Reason: Service Disconnected");
         }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        buildFitnessClient();
     }
 
     public void subscribe() {
@@ -131,5 +146,47 @@ public class MainActivity extends InjectedActivity implements GoogleApiClient.Co
 
     private void readData() {
 
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.w(TAG, "onConnectionFailed");
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_RESOLVE_ERR) {
+            if (resultCode == RESULT_OK) {
+                Log.i(TAG, "Successfully subscribed!");
+            } else if (resultCode != RESULT_OK) {
+                if (!googleApiClient.isConnecting() && googleApiClient.isConnected()) {
+                    showErrorDialog(new Runnable() {
+                        @Override
+                        public void run() {
+                            subscribe();
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    private void showErrorDialog(final Runnable ok) {
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setTitle("FitApp");
+        adb.setMessage("You should accept all the pemissions to use the app");
+        adb.setIcon(android.R.drawable.ic_dialog_alert);
+        adb.setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                ok.run();
+            }
+        });
+        adb.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        adb.show();
     }
 }
