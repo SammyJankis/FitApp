578d4a3bc96ddc8d94aa3f7ffcdf55a467411e42
Arturo Guillen <arturo.guillen@privalia.com>
2017-06-13 17:02:48 +0200

    Added GoalsApi

---
+++ app/src/main/java/com/arturoguillen/fitapp/di/api/GoalsApi.java
@@ -0,0 +1,19 @@
+package com.arturoguillen.fitapp.di.api;
+
+import com.arturoguillen.fitapp.entities.Goal;
+
+import java.util.ArrayList;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
+/**
+ * Created by arturo.guillen on 13/06/2017.
+ */
+
+public interface GoalsApi {
+
+    @GET("v2/593fe033100000be0acd115c")
+    Observable<ArrayList<Goal>> getGoals();
+
+}
