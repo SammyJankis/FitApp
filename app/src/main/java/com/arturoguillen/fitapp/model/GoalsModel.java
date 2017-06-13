e49fd9c865498c45844ce1fa5658e3d3adc04334
Arturo Guillen <arturo.guillen@privalia.com>
2017-06-13 17:37:59 +0200

    Added GoalsModel functionality

---
+++ app/src/main/java/com/arturoguillen/fitapp/model/GoalsModel.java
@@ -0,0 +1,29 @@
+package com.arturoguillen.fitapp.model;
+
+import com.arturoguillen.fitapp.di.api.GoalsApi;
+import com.arturoguillen.fitapp.entities.Goal;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by arturo.guillen on 13/06/2017.
+ */
+
+public class GoalsModel {
+
+    @Inject
+    public GoalsApi goalsApi;
+
+    public Observable<List<Goal>> getGoalsObservable() {
+
+        return goalsApi.getGoals().
+                subscribeOn(Schedulers.io()).
+                observeOn(AndroidSchedulers.mainThread());
+    }
+}
--- app/src/main/java/com/arturoguillen/fitapp/presenter/GoalsPresenter.java
+++ app/src/main/java/com/arturoguillen/fitapp/presenter/GoalsPresenter.java
@@ -1,6 +1,17 @@
 package com.arturoguillen.fitapp.presenter;
 
+import com.arturoguillen.fitapp.R;
+import com.arturoguillen.fitapp.entities.Goal;
+import com.arturoguillen.fitapp.model.GoalsModel;
+import com.arturoguillen.fitapp.utils.LogUtils;
 import com.arturoguillen.fitapp.view.list.GoalListView;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 
 /**
  * Created by arturo.guillen on 13/06/2017.
@@ -8,17 +19,52 @@ import com.arturoguillen.fitapp.view.list.GoalListView;
 
 public class GoalsPresenter implements PresenterInterface<GoalListView> {
 
+    public static final String TAG = GoalsPresenter.class.getSimpleName();
+
+    private GoalListView view;
+
+    private Disposable getGoalsDisposable;
+
+    @Inject
+    public GoalsModel goalsModel;
+
     @Override
     public void attachView(GoalListView view) {
-
+        this.view = view;
     }
 
     @Override
     public void detachView() {
-
+        this.view = null;
+        if (getGoalsDisposable != null)
+            getGoalsDisposable.dispose();
     }
 
     public void getGoalsList() {
+        view.showProgressFooter();
+        getGoalsDisposable = goalsModel.
+                getGoalsObservable().
+                subscribeWith(new DisposableObserver<List<Goal>>() {
+                    @Override
+                    public void onNext(List<Goal> goalList) {
+                        view.hideProgressFooter();
+                        view.hideMessage();
+                        view.showMoreData(goalList);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        view.hideProgressFooter();
+                        view.showMessage(R.string.try_again);
+                        LogUtils.DEBUG(TAG, e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
 
     }
 }
