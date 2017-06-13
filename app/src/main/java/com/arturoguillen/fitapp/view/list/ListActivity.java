ddb3f8e57a2d58652d4edb0385b65dec87955d65
Arturo Guillen <arturo.guillen@privalia.com>
2017-06-13 16:58:20 +0200

    Added ListActivity

--- app/src/main/java/com/arturoguillen/fitapp/view/list/ListActivity.java
+++ app/src/main/java/com/arturoguillen/fitapp/view/list/ListActivity.java
@@ -1,10 +1,25 @@
 package com.arturoguillen.fitapp.view.list;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.arturoguillen.fitapp.R;
+import com.arturoguillen.fitapp.di.FitComponent;
+import com.arturoguillen.fitapp.view.InjectedActivity;
 
 /**
  * Created by arturo.guillen on 13/06/2017.
  */
 
-public class ListActivity extends AppCompatActivity {
+public class ListActivity extends InjectedActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+
+    @Override
+    protected void injectComponent(FitComponent component) {
+        component.inject(this);
+    }
 }
