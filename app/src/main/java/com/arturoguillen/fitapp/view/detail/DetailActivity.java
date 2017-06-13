f106b8a30153021a70ae4b1745bc952964a2d82a
Arturo Guillen <sammyjankis87@gmail.com>
2017-06-13 21:21:30 +0200

    Included some methods to test api functionality

--- app/src/main/java/com/arturoguillen/fitapp/view/detail/DetailActivity.java
+++ app/src/main/java/com/arturoguillen/fitapp/view/detail/DetailActivity.java
@@ -4,36 +4,54 @@ import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 
 import com.arturoguillen.fitapp.R;
 import com.arturoguillen.fitapp.di.component.FitComponent;
 import com.arturoguillen.fitapp.entities.Goal;
+import com.arturoguillen.fitapp.presenter.DetailFitPresenter;
 import com.arturoguillen.fitapp.utils.LogUtils;
 import com.arturoguillen.fitapp.view.PermissionsActivity;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.result.DataReadResult;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
+
+import static java.text.DateFormat.getTimeInstance;
 
 /**
  * Created by agl on 11/06/2017.
  */
 
-public class DetailActivity extends PermissionsActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class DetailActivity extends PermissionsActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        DetailGoalView {
 
-    public static final String TAG = DetailActivity.class.getSimpleName();
+    private static final String TAG = DetailActivity.class.getSimpleName();
     private static int REQUEST_CODE_RESOLVE_ERR = 1000;
     private static final String EXTRA_GOAL = "EXTRA_GOAL";
 
     @Inject
     GoogleApiClient googleApiClient;
+
+    @Inject
+    DetailFitPresenter presenter;
 
     @Override
     public String getTag() {
@@ -61,7 +79,15 @@ public class DetailActivity extends PermissionsActivity implements GoogleApiClie
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        presenter.attachView(this);
+
         setContentView(R.layout.activity_detail);
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.detachView();
+        super.onDestroy();
     }
 
     @Override
@@ -105,7 +131,7 @@ public class DetailActivity extends PermissionsActivity implements GoogleApiClie
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         LogUtils.DEBUG(TAG, "Connected");
-        subscribe();
+        presenter.queryData();
     }
 
     @Override
@@ -132,6 +158,7 @@ public class DetailActivity extends PermissionsActivity implements GoogleApiClie
                     showErrorDialog(new Runnable() {
                         @Override
                         public void run() {
+                            presenter.queryData();
                         }
                     });
                 }
@@ -163,4 +190,60 @@ public class DetailActivity extends PermissionsActivity implements GoogleApiClie
         return intent;
     }
 
+    public static void printData(DataReadResult dataReadResult) {
+        if (dataReadResult.getBuckets().size() > 0) {
+            LogUtils.DEBUG(TAG, "Number of returned buckets of DataSets is: "
+                    + dataReadResult.getBuckets().size());
+            for (Bucket bucket : dataReadResult.getBuckets()) {
+                List<DataSet> dataSets = bucket.getDataSets();
+                for (DataSet dataSet : dataSets) {
+                    dumpDataSet(dataSet);
+                }
+            }
+        } else if (dataReadResult.getDataSets().size() > 0) {
+            LogUtils.DEBUG(TAG, "Number of returned DataSets is: "
+                    + dataReadResult.getDataSets().size());
+            for (DataSet dataSet : dataReadResult.getDataSets()) {
+                dumpDataSet(dataSet);
+            }
+        }
+    }
+
+    private static void dumpDataSet(DataSet dataSet) {
+        LogUtils.DEBUG(TAG, "Data returned for Data type: " + dataSet.getDataType().getName());
+        DateFormat dateFormat = getTimeInstance();
+
+        for (DataPoint dp : dataSet.getDataPoints()) {
+            LogUtils.DEBUG(TAG, "Data point:");
+            LogUtils.DEBUG(TAG, "\tType: " + dp.getDataType().getName());
+            LogUtils.DEBUG(TAG, "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            LogUtils.DEBUG(TAG, "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
+            for (Field field : dp.getDataType().getFields()) {
+                LogUtils.DEBUG(TAG, "\tField: " + field.getName() +
+                        " Value: " + dp.getValue(field));
+            }
+        }
+    }
+
+    @Override
+    public void showData(DataReadResult dataResult) {
+        printData(dataResult);
+    }
+
+    @Override
+    public void requestPermissions(Status status) {
+        LogUtils.DEBUG(TAG, "There was a problem subscribing.");
+        if (status.hasResolution()) {
+            try {
+                status.startResolutionForResult(DetailActivity.this, REQUEST_CODE_RESOLVE_ERR);
+            } catch (IntentSender.SendIntentException e) {
+                LogUtils.DEBUG(TAG, e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void unRegister() {
+        unregisterGoogleApiClientCallbacks();
+    }
 }
