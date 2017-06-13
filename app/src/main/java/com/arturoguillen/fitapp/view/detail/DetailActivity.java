5692b761d20b9bcce082d57ab01006cc13378089
Arturo Guillen <arturo.guillen@privalia.com>
2017-06-13 16:58:40 +0200

    Reformatted code

--- app/src/main/java/com/arturoguillen/fitapp/view/detail/DetailActivity.java
+++ app/src/main/java/com/arturoguillen/fitapp/view/detail/DetailActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.app.AlertDialog;
 import com.arturoguillen.fitapp.R;
 import com.arturoguillen.fitapp.di.FitComponent;
 import com.arturoguillen.fitapp.utils.LogUtils;
-import com.arturoguillen.fitapp.view.InjectedActivity;
 import com.arturoguillen.fitapp.view.PermissionsActivity;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
